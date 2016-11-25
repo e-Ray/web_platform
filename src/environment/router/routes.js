de7146495c578dd93f6f5cccd28482fa7c95f4b9
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2016-11-25 11:55:06 +0100

    Routing anfänge -> Dashboard .gitignore: node_modules hinzugefügt

---
+++ src/environment/router/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router, Route} from 'react-router';
+
+import App from '../../ui/components/generic/App/App';
+import Dashboard from '../../ui/components/generic/Dashboard/Dashboard';
+
+const Routes = (props) => (
+	<Router {...props}>
+		<Route path="/" component={App}/>
+		<Route path="/Dashboard" component={Dashboard}/>
+	</Router>
+);
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { browserHistory} from 'react-router';
+
+import Routes from './environment/router/routes';
+
 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
