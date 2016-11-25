6f6006a27712018260a43833323f235cca86a62d
lukas <lukasstrassel@googlemail.com>
2016-11-25 13:25:52 +0100

    fix router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { browserHistory} from 'react-router';
 import Routes from './environment/router/routes';
 
 class App extends Component {
   render() {
     return (
-      <Routes history={browserHistory} />
+      <Routes />
     );
   }
 }
--- src/environment/router/routes.js
+++ src/environment/router/routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Router, Route} from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from '../../ui/components/generic/App/App';
 import Dashboard from '../../ui/components/generic/Dashboard/Dashboard';
 
-const Routes = (props) => (
-	<Router {...props}>
+const Routes = () => (
+	<Router history={ browserHistory }>
 		<Route path="/" component={App}/>
 		<Route path="/Dashboard" component={Dashboard}/>
 	</Router>
 );
+
 export default Routes;
