0724f8d4fd420395847f27651fcc631c39730525
Anja Kirchhoefer <anja-kirchhoefer@web.de>
2016-12-02 16:35:13 +0100

    Gerüst erstellt
    - bearbeitet #16

--- src/ui/components/generic/_detailPage.js
+++ src/ui/components/generic/_detailPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { LineExample } from '../charts';
+import { WaterLevel } from '../sensors';
+
+function renderSensor(props) {
+  switch(props) {
+    case "waterlevel":
+      return <WaterLevel />;
+    default:
+      return <h2>Fehler: No such sensor</h2>;
+    };
+};
 
 export default React.createClass({
-  displayName: 'DetailPage',
 
   render() {
     return (
       <div>
         <h1>I am a detail page</h1>
+        {renderSensor(this.props.sensor)}
       </div>
     );
   }
---
+++ src/ui/components/sensors/index.js
@@ -0,0 +1,1 @@
+export {default as WaterLevel} from './_waterLevel';
