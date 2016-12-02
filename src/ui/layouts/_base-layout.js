1d650aa4cd413712960bb7d0d3a402cd5beb3eb1
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2016-12-02 20:55:12 +0100

    Moved Menu to _base-layout.js where it belongs.
    - TODO: Make Menu interactive: e.g.: Press Button to show/dismiss Menu

--- src/ui/layouts/_base-layout.js
+++ src/ui/layouts/_base-layout.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MenuExampleIcons from '../components/generic/_menu';
+import AppBarExampleIcon from '../components/generic/_bar';
 
 
 const BaseLayout = ({children}) => (
   <div>
-    <div>I am the baselayout header with <Link to="/dashboard">a Link</Link> to the dashboard</div>
+    <MuiThemeProvider>
+    <AppBarExampleIcon/>
+  	</MuiThemeProvider>
+  	<MuiThemeProvider>
+    <MenuExampleIcons/>
+  </MuiThemeProvider>
     {children}
    
     <div>I am the footer</div>
