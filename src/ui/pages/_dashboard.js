79636ac8f27a786c208188ac64e888eb2470ec97
Anja Kirchhoefer <anja-kirchhoefer@web.de>
2016-12-30 15:23:42 +0100

    Grid für Dasboard in CSS geschrieben
    - improves #13

--- src/ui/pages/_dashboard.js
+++ src/ui/pages/_dashboard.js
@@ -4,20 +4,39 @@ import { PHVal, WaterLevel} from '../components/sensors';
 import { Link } from 'react-router';
 
 
-
 class Dashboard extends Component {
 
 
 
 	render() {
 
-
 		return(
-			<div id="dashboard">
-				<RaisedButton label="Wasserstand" primary={true} containerElement={<Link to="/DetailPageWL" />} />
-        		<WaterLevel  />
-        		<RaisedButton label="PH-Wert" primary={true} containerElement={<Link to="/DetailPagePH" />} />
-        		<PHVal />
+			<div id="container">
+			<div id="row">
+					{/**<div class="col-2" id="graph_waterlevel">
+						<div id="buttonHeader">
+							{/**<RaisedButton label="Wasserstand" fullWidth={true} primary={true} containerElement={<Link to="/DetailPageWL" />} />}
+						</div>
+					{/**<WaterLevel  />}
+					</div>
+
+					<div class="col-2" id="graph_phval">
+						<div id="buttonHeader">
+        			{/**<RaisedButton label="PH-Wert" fullWidth={true} primary={true} containerElement={<Link to="/DetailPagePH" />} />}
+						</div>
+					{/**<PHVal />}
+					</div>**/}
+					<div id="col-2"><p>col-2</p></div>
+					<div id="col-2"><p>col-2</p></div>
+			</div>
+			<div id="row">
+					<div id="col-1"><p>col-1</p></div>
+			</div>
+			<div id="row">
+					<div id="col-3"><p>col-3</p></div>
+					<div id="col-3"><p>col-3</p></div>
+					<div id="col-3"><p>col-3</p></div>
+			</div>
 			</div>
 		);
 
