e025ce493e1f9a14df86c1124e45441d2c8d9668
Anja Kirchhoefer <anja-kirchhoefer@web.de>
2017-01-02 11:03:57 +0100

    Grid für Dashboard
    - improves #21

--- src/ui/pages/_dashboard.js
+++ src/ui/pages/_dashboard.js
@@ -12,31 +12,23 @@ class Dashboard extends Component {
 
 		return(
 			<div id="container">
-			<div id="row">
-					{/**<div class="col-2" id="graph_waterlevel">
+
+				<div id="row">
+					<div id="col-2-left">
 						<div id="buttonHeader">
-							{/**<RaisedButton label="Wasserstand" fullWidth={true} primary={true} containerElement={<Link to="/DetailPageWL" />} />}
+							<RaisedButton label="Wasserstand" fullWidth={true} primary={true} containerElement={<Link to="/DetailPageWL" />} />
 						</div>
-					{/**<WaterLevel  />}
+						<WaterLevel  />
 					</div>
 
-					<div class="col-2" id="graph_phval">
+					<div id="col-2-right">
 						<div id="buttonHeader">
-        			{/**<RaisedButton label="PH-Wert" fullWidth={true} primary={true} containerElement={<Link to="/DetailPagePH" />} />}
+        			<RaisedButton label="PH-Wert" fullWidth={true} primary={true} containerElement={<Link to="/DetailPagePH" />} />
 						</div>
-					{/**<PHVal />}
-					</div>**/}
-					<div id="col-2"><p>col-2</p></div>
-					<div id="col-2"><p>col-2</p></div>
-			</div>
-			<div id="row">
-					<div id="col-1"><p>col-1</p></div>
-			</div>
-			<div id="row">
-					<div id="col-3"><p>col-3</p></div>
-					<div id="col-3"><p>col-3</p></div>
-					<div id="col-3"><p>col-3</p></div>
-			</div>
+						<PHVal />
+					</div>
+				</div>
+
 			</div>
 		);
 
