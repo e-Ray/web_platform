d82e1231209033a476167e9a5a38eb3874b00cf9
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2017-02-22 12:39:15 +0100

    Infoseite Fix
    - #40

--- src/ui/pages/_info.js
+++ src/ui/pages/_info.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import { firebaseAuth } from '../../api/Auth/_constants';
 import { ref } from '../../api/Auth/_constants';
-import { observable, autorun } from 'mobx';
+import Loader from 'react-loader';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
-function getInfo(info) {
-
-    /*ref.child('/erays/'+this.eray+'/info/'+info)
-    .on('value',(snapshot) =>{
-      result = snapshot.val();
-      console.log(snapshot.val());
-    });*/
-
-  return <h5>Für diesen Wert gibt es noch keine Informationen</h5>;
-}
-
+@observer
 class InfoPage extends Component {
 
+  @observable ort = "";
   @observable eray = "";
+  @observable inbetriebnahme = "";
 	constructor(props){
 		super(props);
-		ref.child('/users/'+firebaseAuth().currentUser.uid+'/erays/eray1')
+		ref.child('/users/'+firebaseAuth().currentUser.uid+'/erays/')
 		.on('value',(snapshot) =>{
-			this.eray = snapshot.val();
-			console.log(snapshot.val());
+      
+			this.eray = snapshot.val().eray1;
+      ref.child('erays/'+this.eray+'/info/').on('value',(snapshot) =>{
+        this.ort = snapshot.val().location;
+        this.inbetriebnahme = snapshot.val().startDate;
+      });
+			
 		});
-		autorun(()=>{console.log(this.eray)});
   }
 
 
   render() {
+
+    if(this.location !== ""){
     return (
 
       <div id="container">
@@ -36,16 +36,19 @@ class InfoPage extends Component {
         <h2>Hier gibt es demnächst Informationen zum e.Ray, wie zum Beispiel der Standort, Foto und Datum der Inbetriebnahme.</h2>
         <div id="row">
           <div id="col-5">
-            <h4>Standort: </h4>
-          </div>
-          <div id="col-3">
-            { getInfo("bla") }
+            <h4>Standort: {this.ort} </h4>
+            <h4>Inbetriebnahme: {this.inbetriebnahme} </h4>
           </div>
 
+          <div>
+            Bild
+          </div>
         </div>
 
       </div>
     );
+  }
+  return <Loader loaded={false}/>;
   }
 }
 
