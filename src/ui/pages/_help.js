814ed874f4281bb12c88023c910c54bc6507169a
Anja Kirchhoefer <anja-kirchhoefer@web.de>
2017-02-22 13:27:59 +0100

    Bug-fixes Helppage
    -improves #40

--- src/ui/pages/_help.js
+++ src/ui/pages/_help.js
@@ -9,7 +9,7 @@ class HelpPage extends Component {
 
       <div id="container">
         <h1 id="help0">Hilfe</h1>
-        <h2>Im Nachfolgenden finden sie Hilfe zur Bedienung der Webapplikation. Klicken Sie auf einen Eintrag um dafür Hilfe zu erhalten.</h2>
+        <h4>Im Nachfolgenden finden sie Hilfe zur Bedienung der Webapplikation. Klicken Sie auf einen Eintrag um dafür Hilfe zu erhalten.</h4>
         <div id="row">
           <h3><a href="#help1"> Dashboard - Werte ablesen
           </a></h3>
@@ -27,12 +27,12 @@ class HelpPage extends Component {
 
         <div id="row">
             <div id="help1">
-              <h2>Dashboard - Werte ablesen</h2><br />
+              <h4>Dashboard - Werte ablesen</h4><br />
               <br />
-              <p id="helppassage">Auf dem Dashboard haben Sie als Benutzer verschiedene Möglichkeiten ihre Werte gezielt abzulessen.<br />
-              Es werden ihnen alle acht Sensoren angezeigt und Sie können einfach wie in Abbildung 1 mit der Maus für das gewünschte Datum im Graphen den Wert ablesen. <br />
+              <p id="helppassage">Auf dem Dashboard haben Sie als Benutzer verschiedene Möglichkeiten ihre Werte gezielt abzulesen.<br />
+              Es werden ihnen alle acht Sensoren angezeigt und Sie können wie in Abbildung 1 mit der Maus für das gewünschte Datum im Graphen den Wert ablesen. <br />
               <br />
-              <img src="help1.jpg" id="helpimage"/> <br />
+              <img src="help1.png" id="helpimage"/> <br />
               <br />
               Für detailliertere Informationen müssen Sie die Detailseite des entsprechenden Sensors aufrufen. Dies ist möglich per Mausklick auf die Titelleiste des Sensors (Abbildung 2) oder durch die Verwendung des Menüs in der oberen, linken Ecke.<br />
               <br />
@@ -44,12 +44,12 @@ class HelpPage extends Component {
         </div>
         <div id="row">
             <div id="help2">
-              <h2>Detailseite - Zeitraum einstellen</h2><br />
+              <h4>Detailseite - Zeitraum einstellen</h4><br />
               <br />
               <p id="helppassage">Auf den Detailseiten können Sie detailliertere Zeiträume einstellen um so genau zu sehen, welchen Wert der Sensor an einem bestimmten Datum gemessen hat.<br />
                 Um einen Zeitraum einzustellen müssen Sie auf den Knopf in der oberen rechten Ecke (Abbildung 3) drücken und können anschließend aus dem Dropdown-Menü eine Option auswählen.<br />
                 <br />
-                <img src="help3.jpg" id="helpimage"/> <br />
+                <img src="help3.png" id="helpimage"/> <br />
                 <br />
                 Sie haben ebenfalls die Möglichkeit einen selbst festgelegten Zeitraum einzustellen. Hierfür wählen Sie zunächst aus dem Dropdown-Menü die Option "Custom". Anschließend können Sie in den über dem Graphen befindlichen Feldern ihr gewünschtes Datum wie im Folgenden gezeigt auswählen:<br />
                 <br />
@@ -64,7 +64,7 @@ class HelpPage extends Component {
         </div>
         <div id="row">
             <div id="help3">
-              <h2>Logout</h2><br />
+              <h4>Logout</h4><br />
               <br />
               <p id="helppassage">Um sich auszuloggen können Sie jederzeit auf den "Logout"-Knopf in der oberen, rechten Ecke klicken und werden automatisch ausgeloggt.<br />
               <br /></p>
