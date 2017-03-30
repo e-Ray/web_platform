159ae96dc97e252704c91e00a63866e176576f3c
Kay Wallaschek <kayluis.wallaschek@stud.tu-darmstadt.de>
2017-03-30 11:44:04 +0200

    codeClimate fixes

--- src/ui/pages/index.js
+++ src/ui/pages/index.js
@@ -93,37 +93,48 @@ export default class App extends Component {
                 <MatchWhenUnauthed authed={this.state.authed} pattern="/" component={Login} />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/dashboard" component={Dashboard} />
+                  pattern="/dashboard" component={Dashboard}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPagePerf" component={PERF} />
+                  pattern="/detailPagePerf" component={PERF}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageRain" component={RAIN} />
+                  pattern="/detailPageRain" component={RAIN}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageRpm" component={RPM} />
+                  pattern="/detailPageRpm" component={RPM}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageTemp" component={TEMP} />
+                  pattern="/detailPageTemp" component={TEMP}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageWaterTemp" component={WTEMP} />
+                  pattern="/detailPageWaterTemp" component={WTEMP}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageWindDir" component={WDIR} />
+                  pattern="/detailPageWindDir" component={WDIR}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageWindSpeed" component={WSPEED} />
+                  pattern="/detailPageWindSpeed" component={WSPEED}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/detailPageWL" component={WL} />
+                  pattern="/detailPageWL" component={WL}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/InfoPage" component={InfoPage} />
+                  pattern="/InfoPage" component={InfoPage}
+                />
                 <MatchWhenAuthed
                   authed={this.state.authed}
-                  pattern="/HelpPage" component={HelpPage} />
+                  pattern="/HelpPage" component={HelpPage}
+                />
                 <Miss render={() => <h3>No Match</h3>} />
               </div>
             </div>
